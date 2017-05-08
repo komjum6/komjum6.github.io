06f9ade01f76e6f5aa3f345db5f84f4e2a82d6f0
komjum6 <jphh56@gmail.com>
2017-05-08 13:16:54 +0200

    Website upload version 1.0

---
+++ FlaskFile.py
@@ -0,0 +1,53 @@
+from flask import Flask, render_template
+from wtforms import Form, BooleanField, StringField, PasswordField, validators
+app = Flask(__name__, static_url_path='',static_folder="WiWebSite")
+
+webcode = open('WiWebSite.html').read()
+registerfile = open('register.html').read()
+
+@app.route('/')
+def webprint():
+    return webcode
+
+#Hier is een experimentele aanpak
+
+
+class RegistrationForm(Form):
+    username = StringField('Username', [validators.Length(min=4, max=25)])
+    email = StringField('Email Address', [validators.Length(min=6, max=35)])
+    password = PasswordField('New Password', [
+        validators.DataRequired(),
+        validators.EqualTo('confirm', message='Passwords must match')
+    ])
+    confirm = PasswordField('Repeat Password')
+    accept_tos = BooleanField('I accept the TOS', [validators.DataRequired()])
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm(request.form)
+    if request.method == 'POST' and form.validate():
+        user = User(form.username.data, form.email.data,
+                    form.password.data)
+        db_session.add(user)
+        flash('Thanks for registering')
+        return redirect(url_for('login'))
+    return registerfile
+
+#Hier eindigt de experimentele aanpak
+
+if __name__ == "__FlaskFile__":
+    app.run(debug=False)
+
+"""    
+set FLASK_APP=FlaskFile.py
+py -m flask run
+
+url_for('static', filename='style.css')
+
+name=None
+render_template("WiWebSite.html", name=name)
+
+
+
+return render_template('register.html', form=form)
+"""
---
+++ FlaskFile.wsgi
@@ -0,0 +1,3 @@
+import sys
+sys.path.insert(0, '/home/owe1_pg18/public_html/Justin')
+from FlaskFile import app as application
